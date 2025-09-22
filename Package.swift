f749faad4174c687219bac453932cadab67e978e
Tom Estelrich <tom.estelrich@cultforapps.com>
2025-09-22 18:37:01 +0200

    VLCKitVersion.current added

--- Package.swift
+++ Package.swift
@@ -19,52 +19,50 @@ enum VLCKitVersion: String {
 
 // MARK: - Package
 
-var package: Package {
-    switch VLCKitVersion.current {
-    case .v3_6_1:
-        return Package(
-            name: "VLCKit",
-            platforms: [.iOS(.v17), .tvOS(.v17)],
-            products: [.library(name: "VLCKit", targets: ["VLCKitBridge"])],
-            targets: [
-                .binaryTarget(
-                    name: "MobileVLCKit",
-                    url: "https://github.com/EstelrichAppLabs/VLCKitPackage/releases/download/2025.7.0/MobileVLCKit.xcframework.zip",
-                    checksum: "f22ec82dcf2e5b6cc6819cc787767561d9eed8281df68679f05d598f53ead5a6"
-                ),
-                .binaryTarget(
-                    name: "TVVLCKit",
-                    url: "https://github.com/EstelrichAppLabs/VLCKitPackage/releases/download/2025.7.0/TVVLCKit.xcframework.zip",
-                    checksum: "f4b1a3dcee5a4144cb7050cf31d4243a293d97a674f39ff6d4bf534ac8942825"
-                ),
-                .target(
-                    name: "VLCKitBridge",
-                    dependencies: [
-                        .target(name: "MobileVLCKit", condition: .when(platforms: [.iOS])),
-                        .target(name: "TVVLCKit", condition: .when(platforms: [.tvOS]))
-                    ],
-                    path: "Sources/VLCKit",
-                    swiftSettings: [.define("VLCKIT_LEGACY")]
-                )
-            ]
-        )
-    case .v4_0_0:
-        return Package(
-            name: "VLCKit",
-            platforms: [.iOS(.v17), .tvOS(.v17)],
-            products: [.library(name: "VLCKit", targets: ["VLCKitBridge"])],
-            targets: [
-                .binaryTarget(
-                    name: "VLCKitBinary",
-                    url: "https://github.com/EstelrichAppLabs/VLCKitPackage/releases/download/2025.9.0/VLCKit.xcframework.zip",
-                    checksum: "01c9e647e6dea078e7586242fe23020e5a505e73ca312eac813d0d8198c9dd03"
-                ),
-                .target(
-                    name: "VLCKitBridge",
-                    dependencies: ["VLCKitBinary"],
-                    path: "Sources/VLCKit"
-                )
-            ]
-        )
-    }
+switch VLCKitVersion.current {
+case .v3_6_1:
+    let package = Package(
+        name: "VLCKit",
+        platforms: [.iOS(.v17), .tvOS(.v17)],
+        products: [.library(name: "VLCKit", targets: ["VLCKitBridge"])],
+        targets: [
+            .binaryTarget(
+                name: "MobileVLCKit",
+                url: "https://github.com/EstelrichAppLabs/VLCKitPackage/releases/download/2025.7.0/MobileVLCKit.xcframework.zip",
+                checksum: "f22ec82dcf2e5b6cc6819cc787767561d9eed8281df68679f05d598f53ead5a6"
+            ),
+            .binaryTarget(
+                name: "TVVLCKit",
+                url: "https://github.com/EstelrichAppLabs/VLCKitPackage/releases/download/2025.7.0/TVVLCKit.xcframework.zip",
+                checksum: "f4b1a3dcee5a4144cb7050cf31d4243a293d97a674f39ff6d4bf534ac8942825"
+            ),
+            .target(
+                name: "VLCKitBridge",
+                dependencies: [
+                    .target(name: "MobileVLCKit", condition: .when(platforms: [.iOS])),
+                    .target(name: "TVVLCKit", condition: .when(platforms: [.tvOS]))
+                ],
+                path: "Sources/VLCKit",
+                swiftSettings: [.define("VLCKIT_LEGACY")]
+            )
+        ]
+    )
+default:
+    let package = Package(
+        name: "VLCKit",
+        platforms: [.iOS(.v17), .tvOS(.v17)],
+        products: [.library(name: "VLCKit", targets: ["VLCKitBridge"])],
+        targets: [
+            .binaryTarget(
+                name: "VLCKitBinary",
+                url: "https://github.com/EstelrichAppLabs/VLCKitPackage/releases/download/2025.9.0/VLCKit.xcframework.zip",
+                checksum: "01c9e647e6dea078e7586242fe23020e5a505e73ca312eac813d0d8198c9dd03"
+            ),
+            .target(
+                name: "VLCKitBridge",
+                dependencies: ["VLCKitBinary"],
+                path: "Sources/VLCKit"
+            )
+        ]
+    )
 }
